73fb3a8dd96f4db88cdf8b49d542a9cde0fdbddc
vinchawla <vinchawla78@gmail.com>
2020-02-01 11:40:16 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    let user = {
+	first: 'Vin',
+	last: 'Chawla',
+    };
+    var helloWorld = 'Welcome to the road to learn React!';
+    return (
+	    <div className="App">
+	    <h2>{helloWorld} {user.first} {user.last}!!!</h2>
+	    </div>
+    );
+}
+
+export default App;
